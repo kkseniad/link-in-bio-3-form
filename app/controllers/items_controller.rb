26751d693f4d2e325e95b04187740093e6b493b4
Kseniia Kormalova <kormalovak@gmail.com>
2025-09-20 22:08:21 +0000

    fixed an error

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,8 +13,8 @@ class ItemsController < ApplicationController
     new_item = Item.new
 
     new_item.link_url = params.fetch("query_link_url")
-    new_item.link_description = params.fetch("query_link_desription")
-    new_item.thumbnail_url = params.fethc("query_thumbnail_url")
+    new_item.link_description = params.fetch("query_link_description")
+    new_item.thumbnail_url = params.fetch("query_thumbnail_url")
 
     new_item.save
 
